82a2b7b76b09c76b74c260f3db302667041861cd
chze10 <166258149+chze10@users.noreply.github.com>
2024-04-06 23:11:53 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+@app.get("/{chzexy}")
+async def root(chzexy):
+    return {"greeting": chzexy, "message": "Welcome to FastAPI!"}
